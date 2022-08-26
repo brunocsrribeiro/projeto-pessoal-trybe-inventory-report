efcb077091bcddfd58d98bfa0806962cfb9f4def
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-08-26 14:01:00 -0300

    Chore: Inicializado projeto e impelementado testes dos requisitos 1 e 2

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Dorflex",
+        nome_da_empresa="Farmacon",
+        data_de_fabricacao="2022-10-20",
+        data_de_validade="2024-10-20",
+        numero_de_serie="123456",
+        instrucoes_de_armazenamento="em local seco"
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == "Dorflex"
+    assert product.nome_da_empresa == "Farmacon"
+    assert product.data_de_fabricacao == "2022-10-20"
+    assert product.data_de_validade == "2024-10-20"
+    assert product.numero_de_serie == "123456"
+    assert product.instrucoes_de_armazenamento == "em local seco"
--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Dorflex",
+        nome_da_empresa="Farmacon",
+        data_de_fabricacao="2022-10-20",
+        data_de_validade="2024-10-20",
+        numero_de_serie="123456",
+        instrucoes_de_armazenamento="em local seco"
+    )
+
+    assert product.__repr__() == (
+        f"O produto {product.nome_do_produto} "
+        f"fabricado em {product.data_de_fabricacao} "
+        f"por {product.nome_da_empresa} com validade "
+        f"até {product.data_de_validade} "
+        f"precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
